59b645e0eff2dcaedbb901107ab0cab274a3d1f1
Jeremy Dryden <drydenje@Jeremys-Air.local>
2024-12-12 15:14:14 -0500

    started on cms, added some colour variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // adding to remove error message "legacy JS API is depreceated"
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern",
+          importers: [],
+        },
+      },
+    },
+  },
+});
